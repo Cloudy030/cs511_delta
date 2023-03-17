09254a779ec39c52a39ef16640e3fef9283ab10f
POK NGA HO <p.ho.22@abdn.ac.uk>
2023-03-17 16:00:11 +0000

    4 models country totla emission percapitaemission source

--- emission/models.py
+++ emission/models.py
@@ -4,8 +4,42 @@ from django.db import models
 # Create your models here.
 
 class Country(models.Model):
-  countryName=models.TextField()
-  countryCode=models.TextField()
+  country_name=models.TextField()
+  country_code=models.TextField()
 
   def __str__(self):
     return f'{self.countryName}, {self.countryCode}'
+
+class TotalEmission(models.Model):
+  country_code=models.ForeignKey('emission.Country', on_delete=models.CASCADE, related_name='totalemission')
+  year=models.IntegerField()
+  total=models.FloatField()
+  coal=models.FloatField()
+  oil=models.FloatField()
+  gas=models.FloatField()
+  cement=models.FloatField()
+  flaring=models.FloatField()
+  percapita=models.FloatField()
+
+  def __str__(self):
+    return f'{self.country_code}, {self.year}, {self.total}, {self.coal}, {self.oil}, {self.gas}, {self.cement}, {self.flaring}, {self.percapita}'
+
+class PerCapitaEmission(models.Model):
+  country_code=models.ForeignKey('emission.Country', on_delete=models.CASCADE, related_name='percapitaemission')
+  year=models.IntegerField()
+  totalpc=models.ForeignKey('emission.TotalEmission', on_delete=models.CASCADE, related_name='totalpercapita')
+  coal=models.FloatField()
+  oil=models.FloatField()
+  gas=models.FloatField()
+  cement=models.FloatField()
+  flaring=models.FloatField()
+
+  def __str__(self):
+    return f'{self.country_code}, {self.year}, {self.total}, {self.coal}, {self.oil}, {self.gas}, {self.cement}, {self.flaring}'
+
+class Source(models.Model):
+  coal=models.TextField()
+  oil=models.TextField()
+  gas=models.TextField()
+  cement=models.TextField()
+  flaring=models.TextField()
