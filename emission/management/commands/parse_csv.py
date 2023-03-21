df1f09f2d2f17a08dbb5df655406d9785cf6baa7
CHINEDU EZEOKOYE <c.ezeokoye3.22@abdn.ac.uk>
2023-03-21 17:32:31 +0000

    updated parse_csv file to read country.csv

--- emission/management/commands/parse_csv.py
+++ emission/management/commands/parse_csv.py
@@ -12,37 +12,46 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         # Drop the data from the tables to initialise the database, soc we can rerun the files without repitition
-        Country.objects.all().delete()
-        TotalEmission.objects.all().delete()
+        #Country.objects.all().delete()
+        #TotalEmission.objects.all().delete()
         PerCapitaEmission.objects.all().delete()
         Source.objects.all().delete()
         print('tables dropped successfully')
 
-        # Open the csv data files to parse them into the database
-        base_dir = Path(__file__).resolve().parent.parent.parent.parent
-        with open(str(base_dir) + '/data/GCB2022v27_MtCO2_flat.csv', newline='') as f:
-            reader = csv.reader(f, delimiter=",")
-            next(reader)    # To skip header line
-            for row in reader:
-                print(row)
+        # # Open the csv data files to parse them into the database
+        # base_dir = Path(__file__).resolve().parent.parent.parent.parent
+        # with open(str(base_dir) + '/data/country.csv', newline='') as f:
+        #     reader = csv.reader(f, delimiter=",")
+        #     next(reader)    # To skip header line
+        #     for row in reader:
+        #         print(row)
 
-                country = Country.objects.create(
-                    country_name = row[0],
-                    country_code =row[1],
-                )
-                country.save()
+        #         country = Country.objects.create(
+        #             country_name = row[0],
+        #             country_code =row[1],
+        #         )
+        #         country.save()
+        # print('Country Table Parsed Successfully')
 
-                totalemission = TotalEmission.objects.create(
-                    year = int(row[2]),
-                    total = float(row[3]),
-                    coal = float(row[4]),
-                    oil = float(row[5]),
-                    gas = float(row[6]),
-                    cement = float(row[7]),
-                    flaring = float(row[8]),
-                )
-                totalemission.save()
+        # # Open the csv data files to parse them into the database
+        # base_dir = Path(__file__).resolve().parent.parent.parent.parent
+        # with open(str(base_dir) + '/data/GCB2022v27_MtCO2_flat.csv', newline='') as f:
+        #     reader = csv.reader(f, delimiter=",")
+        #     next(reader)    # To skip header line
+        #     for row in reader:
+        #         print(row)
 
+        #         totalemission = TotalEmission.objects.create(
+        #             year = int(row[2]),
+        #             total = float(row[3]),
+        #             coal = float(row[4]),
+        #             oil = float(row[5]),
+        #             gas = float(row[6]),
+        #             cement = float(row[7]),
+        #             flaring = float(row[8]),
+        #         )
+        #         totalemission.save()
+        
         base_dir = Path(__file__).resolve().parent.parent.parent.parent
         with open(str(base_dir) + '/data/GCB2022v27_percapita_flat.csv', newline='') as f:
             reader = csv.reader(f, delimiter=",")
@@ -51,7 +60,6 @@ class Command(BaseCommand):
                 print(row)
 
                 percapitaemission = PerCapitaEmission.objects.create(
-                    country_code = row[1],
                     year = int(row[2]),
                     percapita = float(row[3]),
                     coal = float(row[4]),
