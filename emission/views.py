7a53e0daed204a325e104a9c373b13baea45ad8f
TASNIM FATEHA <t.fateha.22@abdn.ac.uk>
2023-03-26 00:34:27 +0000

    added pagination to the table

--- emission/views.py
+++ emission/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Country, TotalEmission, PerCapitaEmission, Source
 
 # Create your views here.
@@ -15,6 +16,22 @@ def total(request):
 def totalemission(request, format=None):
     totalemissions = TotalEmission.objects.all()
 
+    # Get the current page number from the request's GET parameters
+    page_number = request.GET.get('page')
+
+    # Create a Paginator object that contains the totalemissions
+    paginator = Paginator(totalemissions, 10) # Show 10 totalemissions per page
+
+    try:
+        # Get the Page object for the current page number
+        page_obj = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer, show the first page
+        page_obj = paginator.get_page(1)
+    except EmptyPage:
+        # If page_number is out of range (e.g. 9999), show the last page
+        page_obj = paginator.get_page(paginator.num_pages)
+
     if format == 'map':
         # Render map template
         return render(request, 'emission/totalemission_map.html', {'totalemissions': totalemissions, 'format': format})
@@ -23,7 +40,8 @@ def totalemission(request, format=None):
         return render(request, 'emission/totalemission_chart.html', {'totalemissions': totalemissions, 'format': format})
     else:
         # Render table template by default
-        return render(request, 'emission/totalemission.html', {'totalemissions': totalemissions, 'format': format})
+        return render(request, 'emission/totalemission.html', {'page_obj': page_obj, 'format': format})
+
 
 
 #def totalemission(request):
