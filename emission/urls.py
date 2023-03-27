2ef93e0e8fb42d030a47d1be37310f831b719704
TASNIM FATEHA <t.fateha.22@abdn.ac.uk>
2023-03-27 11:59:16 +0000

    changing writing in percapita and total emission page

--- emission/urls.py
+++ emission/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     path('country_list', views.country_list, name='country_list'),
     path('totalemission', views.totalemission, name='totalemission'),
     path('total/', views.total, name='total'),
-path('total/map/', views.totalemission, {'format': 'map'}, name='totalemission_map'),
-
+    path('total/map/', views.totalemission, {'format': 'map'}, name='totalemission_map'),
+    path('per_capita/', views.per_capita, name='per_capita'),
+    path('per_capita/map/', views.percapitaemission, {'format': 'map'}, name='per_capita_map'),
+    path('per_capita/chart/', views.percapitaemission, {'format': 'chart'}, name='per_capita_chart'),
     path('percapitaemission', views.percapitaemission, name='percapitaemission'),
     path('source', views.source, name='source'),
 ]
--- emission/views.py
+++ emission/views.py
@@ -63,9 +63,57 @@ def totalemission(request, format=None):
   #totalemissions=TotalEmission.objects.all()
   #return render(request, 'emission/totalemission.html',{'totalemissions':totalemissions})
 
-def percapitaemission(request):
-  percapitaemissions=PerCapitaEmission.objects.all()
-  return render(request, 'emission/percapitaemission.html',{'percapitaemissions':percapitaemissions})
+def per_capita(request):
+    return render(request, 'emission/per_capita.html')
+
+def percapitaemission(request, format=None):
+    percapitaemissions = PerCapitaEmission.objects.all()
+
+    # Get the year from the request's GET parameters
+    year = request.GET.get('year')
+
+    # Filter the per capita emissions by year, if year is provided
+    if year:
+        year_emissions = percapitaemissions.filter(year=year)
+    else:
+        # Retrieve per capita emissions for the latest year
+        latest_year = PerCapitaEmission.objects.latest('year').year
+        year_emissions = percapitaemissions.filter(year=latest_year)
+
+    # Get the current page number from the request's GET parameters
+    page_number = request.GET.get('page')
+
+    # Create a Paginator object that contains the percapitaemissions
+    paginator = Paginator(percapitaemissions, 10) # Show 10 percapitaemissions per page
+
+    try:
+        # Get the Page object for the current page number
+        page_obj = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer, show the first page
+        page_obj = paginator.get_page(1)
+    except EmptyPage:
+        # If page_number is out of range (e.g. 9999), show the last page
+        page_obj = paginator.get_page(paginator.num_pages)
+
+    if format == 'map':
+        # Create a list of countries and their per capita emissions for the selected year
+        country_emissions = []
+        for emission in year_emissions:
+            country_emissions.append((emission.country.country_name, emission.percapita))
+        # Render map template
+        return render(request, 'emission/per_capita_map.html', {'country_emissions': country_emissions, 'format': format})
+    elif format == 'chart':
+        # Render chart template
+        return render(request, 'emission/per_capita_chart.html', {'percapitaemissions': percapitaemissions, 'format': format})
+    else:
+        # Render table template by default
+        return render(request, 'emission/percapitaemission.html', {'page_obj': page_obj, 'format': format})
+
+
+#def percapitaemission(request):
+  #percapitaemissions=PerCapitaEmission.objects.all()
+  #return render(request, 'emission/percapitaemission.html',{'percapitaemissions':percapitaemissions})
 
 def source(request):
   sources=Source.objects.all()
